95626afbadac2c77f547a7e6ba8824800349bdd9
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-23 06:34:02 +0530

    add restaurantSchema

---
+++ src/models/restaurant.models.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from "mongoose";
+
+const restaurantSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true
+    },
+
+    foods: {
+        type: Schema.Types.ObjectId,
+        ref: 'foods'
+
+    }
+})
+
+export const RestaurantSchema = mongoose.model('restaurant', restaurantSchema)
