40f79e38ee5a5eac1a321a1c4ebbba9af695789a
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-23 06:18:17 +0530

    add foodSchema

---
+++ src/models/food.models.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose";
+
+const foodSchema =  new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        requried: true
+    },
+    description:{
+        type: String,
+
+    },
+    isAvailable:{
+        type: Boolean,
+        default: true
+    }
+})
+
+export const Food = mongoose.model('food', foodSchema)
